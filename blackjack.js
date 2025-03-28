bcb9a50821db81c8b00f551a6ab0d10228b57f37
hazzyOfficial <fuenteshaziel45@gmail.com>
2025-03-28 16:41:35 +0000

    first commit

--- blackjack.js
+++ blackjack.js
@@ -2,5 +2,22 @@ let playerCount = 0;
 let dealerCount =0;
 
 function startGame() {
-    
+playerCount = getCards() + getCards();
+if (playerCount > 21) {
+    console.log("you lose");
+} else {
+    // console.log("hit or stay?");
+    prompt("hit or stay");
+    if(prompt == "hit") {
+        playerCount = getCards()
+    } else {
+        
+    }
 }
+
+}
+
+function getCards() {
+    return Math.random()*11;
+}
+
